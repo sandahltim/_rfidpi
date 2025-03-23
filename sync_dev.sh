fc0cff7806dd2cde95ad2ab7cb2d6d74ff579c08
sandahltim <tim@broadwaytent.com>
2025-03-23 16:53:56 -0500

    Create sync_dev.sh

---
+++ sync_dev.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd /home/tim/test_rfidpi
+
+# Stash SCP’d files
+git stash push -m "Save SCP’d config and refresh_logic" config.py refresh_logic.py
+
+# Pull latest from dev
+git fetch origin
+git checkout dev_rollback
+git pull origin dev_rollback
+
+# Reapply SCP’d files
+git stash pop
+
+# Restart service
+sudo systemctl restart rfid_dash_test
+
+echo "Sync complete: $(git rev-parse HEAD)"
