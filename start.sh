efe889242bc7319e4a2daf735b0686e7beb06aa8
sandahltim <tim@broadwaytent.com>
2025-03-27 11:51:30 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /home/tim/test_rfidpi/venv/bin/activate
-gunicorn --workers 2 --bind 0.0.0.0:8102 --access-logfile /home/tim/test_rfidpi/logs/gunicorn_access.log --error-logfile /home/tim/test_rfidpi/logs/gunicorn_error.log --log-level debug run:app
+gunicorn --workers 2 --bind 0.0.0.0:8102 --access-logfile /home/tim/test_rfidpi/logs/gunicorn_access.log --error-logfile /home/tim/test_rfidpi/logs/gunicorn_error.log --log-level debug --capture-output --enable-stdio-inheritance run:app
