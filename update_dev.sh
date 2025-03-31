31e6a83756c8420223bfdc1f2aa32caf7625cfb9
sandahltim <tim@broadwaytent.com>
2025-03-30 20:48:21 -0500

    1

--- update_dev.sh
+++ update_dev.sh
@@ -5,4 +5,4 @@ git pull origin refresh_print
 mv config.py.bak config.py
 source venv/bin/activate
 pip install -r requirements.txt
-sudo systemctl restart rfid_dash_test
+sudo systemctl restart rfid_dash
